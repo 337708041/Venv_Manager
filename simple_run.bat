5e1165754414ff6ad603a587e4ffc6b16e29bf00
MAO_NIANG <110524282+337708041@users.noreply.github.com>
2025-07-29 22:44:39 +0800

    Update simple_run.bat
    不再使用虚拟环境运行

--- simple_run.bat
+++ simple_run.bat
@@ -3,4 +3,4 @@ if "%1"=="hide" goto CmdBegin
 start mshta vbscript:createobject("wscript.shell").run("""%~0"" hide",0)(window.close)&&exit
 :CmdBegin
 cd /d "%~dp0"
-venv\Scripts\python.exe main.py
+python.exe main.py
